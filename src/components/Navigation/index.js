c222321c4d1acff147877800e0d7aeab27ac8e23
Ece ÇANGA <44345414+ececanga@users.noreply.github.com>
2021-06-16 16:40:16 +0300

    CSS edited

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -25,14 +25,59 @@ const NavigationAuth = () => (
 );
 
 const NavigationNonAuth = () => (
+    <div style={styles.container} className="container">
+        <a style={styles.brandStyle}
+        className="brand" href="/accounts/">Bilkent University Online Services</a>
+
+            <ul style={styles.linkButtonsStyle} className="pull-right nav" id="yw1" role="menu">
+                <a style={styles.textStyle} href="/accounts/site/language?language=tr"><i
+                className="icon-flag icon-white"/> Türkçe</a>
+                <a style={styles.textStyle} href="/accounts/reset-password"><i
+                className="icon-repeat icon-white"/> Reset password</a>
+                <a style={styles.textStyle} href="/accounts/login"><i
+                className="icon-user icon-white"/> Login</a>
+            </ul>
+
+
   <ul>
-    <li>
-      <Link to={ROUTES.LANDING}>Landing</Link>
+    <li style={styles.textStyle}>
+      <Link style={styles.textStyle} to={ROUTES.LANDING}>Landing</Link>
     </li>
-    <li>
-      <Link to={ROUTES.SIGN_IN}>Sign In</Link>
+    <li style={styles.textStyle}>
+      <Link style={styles.textStyle} to={ROUTES.SIGN_IN}>Sign In</Link>
     </li>
   </ul>
+
+    </div>
 );
- 
+
+let styles = {
+    container: {
+        //paddingTop: 30,
+        backgroundColor: "#003366",
+        display: 'flex',
+        justifyContent: 'center',
+
+    },
+    textStyle: {
+        color: "#ffffff",
+        fontFamily: "Arial",
+        textDecoration: 0,
+        marginRight: 30,
+    },
+    brandStyle: {
+        fontSize: 20,
+        fontWeight: 200,
+        fontFamily: "Arial",
+        color: "#ffffff",
+        textDecoration: 0,
+        padding: 12,
+    },
+    linkButtonsStyle: {
+        //display: 'flex',
+        marginLeft: 500,
+        justifyContent: 'flex-end',
+    },
+};
+
 export default Navigation;
--- src/components/SignIn/index.js
+++ src/components/SignIn/index.js
@@ -1,35 +1,36 @@
 import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
 import { compose } from 'recompose';
- 
+
 import { SignUpLink } from '../SignUp';
 import { withFirebase } from '../Firebase';
 import * as ROUTES from '../../constants/routes';
- 
+
+
 const SignInPage = () => (
-  <div>
+  <div style={styles.container}>
     <h1>SignIn</h1>
     <SignInForm />
     <SignUpLink />
   </div>
 );
- 
+
 const INITIAL_STATE = {
   email: '',
   password: '',
   error: null,
 };
- 
+
 class SignInFormBase extends Component {
   constructor(props) {
     super(props);
- 
+
     this.state = { ...INITIAL_STATE };
   }
- 
+
   onSubmit = event => {
     const { email, password } = this.state;
- 
+
     this.props.firebase
       .doSignInWithEmailAndPassword(email, password)
       .then(() => {
@@ -39,21 +40,37 @@ class SignInFormBase extends Component {
       .catch(error => {
         this.setState({ error });
       });
- 
+
     event.preventDefault();
   };
- 
+
   onChange = event => {
     this.setState({ [event.target.name]: event.target.value });
   };
- 
+
   render() {
     const { email, password, error } = this.state;
- 
+
     const isInvalid = password === '' || email === '';
- 
+
     return (
       <form onSubmit={this.onSubmit}>
+
+          <div style={styles.headerContainer}>
+              <header>
+                  <img style={styles.logoStyle} src="../../bilkent-logo.jpg"/>
+                  <h1 style={styles.bilkentTitleStyle}>
+                      Bilkent University
+                      <span style={styles.subHeader} className="sub-header">Secure Login Gateway</span>
+                  </h1>
+              </header>
+              <legend style={styles.legendStyle}>
+                  Login :
+                  <b>SRS</b> - Student Academic Information Registration System
+              </legend>
+              <div style={styles.divBorder}/>
+          </div>
+
         <input
           name="email"
           value={email}
@@ -71,18 +88,72 @@ class SignInFormBase extends Component {
         <button disabled={isInvalid} type="submit">
           Sign In
         </button>
- 
+
         {error && <p>{error.message}</p>}
       </form>
     );
   }
 }
- 
+
 const SignInForm = compose(
   withRouter,
   withFirebase,
 )(SignInFormBase);
- 
+
 export default SignInPage;
- 
+
 export { SignInForm };
+
+let styles = {
+    container: {
+        //paddingTop: 30,
+        //backgroundColor: "#eee",
+        paddingLeft: 420,
+    },
+    logoStyle: {
+        height: 100,
+        width: 100,
+    },
+    subHeader: {
+        color: "#999",
+        fontSize: 22,
+        lineHeight: 1.4,
+        fontWeight: 'normal',
+        display: 'block',
+    },
+    bilkentTitleStyle: {
+        marginTop: -100,
+        color: "#014a96",
+        paddingLeft: 120,
+    },
+    headerContainer: {
+        display: 'wrap',
+        //justifyContent: 'center',
+        //alignItems: 'left',
+
+    },
+    legendStyle: {
+        fontFamily: 'Arial',
+        display: 'block',
+        width: '100%',
+        padding: 0,
+        marginBottom: 20,
+        fontSize: 21,
+        //lineHeight: 40,
+        color: '#333',
+        border: 0,
+
+    },
+    divBorder: {
+        borderBottom: "1px solid #e5e5e5",
+       // borderBottomColor: '#ff0000',
+        borderColor: "ff0000",
+        borderHeight: 200,
+        //marginLeft: 0,
+        marginRight: 600,
+        marginBottom: 20,
+        size: 20,
+    },
+
+};
+
